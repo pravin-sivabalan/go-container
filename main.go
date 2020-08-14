3bb6b9785354bf7700c6c80637652d490cee529f
Pravin Sivabalan <pravinsivabalan@gmail.com>
2020-08-14 04:57:47 +0000

    Initial uts namespace

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+func main() {
+	cmd := exec.Command("/bin/sh")
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS,
+	}
+
+	syscall.Sethostname([]byte("container"))
+
+	cmd.Run()
+
+}
+
+// NEWUTS - hostname
+
+func child() {
+	cmd := exec.Command("/bin/sh")
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS,
+	}
+
+	cmd.Run()
+}
